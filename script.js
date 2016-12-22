83da5003cae09634090593e5a4ce30768f06b427
Preethi <rpsthecoder@users.noreply.github.com>
2016-12-22 21:09:01 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+try {
+    if (!'MediaSource' in window)
+        throw new ReferenceError('There is no MediaSource property in window object.');
+
+    var mime = 'audio/mpeg';
+
+    if (!MediaSource.isTypeSupported(mime)) {
+        alert('Can not play the media. Media of MIME type' + mime + 'is not supported.');
+        throw ('Media of type' + mime + 'is not supported.');
+    }
+
+    var audio = document.querySelector('audio'),
+        mediaSource = new MediaSource();
+    	
+    audio.src = URL.createObjectURL(mediaSource);
+
+    mediaSource.addEventListener('sourceopen', function() {
+        var sourceBuffer = this.addSourceBuffer(mime);
+        sourceBuffer.appendWindowEnd = 4.0;
+        var xhr = new XMLHttpRequest;
+        xhr.open('GET', 'sample.mp3');
+        xhr.responseType = 'arraybuffer';
+        xhr.onload = function() {
+            try {
+                switch (this.status) {
+                    case 200:
+                        sourceBuffer.appendBuffer(this.response);
+                        sourceBuffer.addEventListener('updateend', function (_) {
+                            mediaSource.endOfStream();
+                        });
+                        break;
+                    case 404:
+                        throw 'File Not Found';
+                    default:
+                        throw 'Failed to fetch the file';
+                }
+            } catch (e) {
+                console.error(e);
+            }
+        };
+        xhr.send();
+    });
+} catch (e) {
+    console.error(e);
+}
